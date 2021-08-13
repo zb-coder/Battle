bed8bd64c07c2e3c852927b037376f5060d2b6dc
zainab baruud <zainabbaruud@ma373.home>
2021-08-13 15:03:43 +0100

    added player names and added html to form

--- app.rb
+++ app.rb
@@ -2,15 +2,21 @@ require 'sinatra'
 
 
 class Battle < Sinatra::Base
-
+enable :sessions #In Sinatra, session is a Hash, and you can set values for its keys. session is most often used to store details of a logged in user.
   get '/' do
     erb :index
   end
 
   post '/names' do 
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb :play
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect '/play'
   end 
 
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb:play
+  end
+  
 end 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,6 +5,9 @@ feature "Enter names" do
     fill_in :player_1, with: 'Zainab'
     fill_in :player_2, with: 'Graeme'
     click_button 'Submit'
+
+    save_and_open_page # will save the web page and open the browser to display it
+    
     expect(page).to have_content 'Zainab vs. Graeme'
   end 
 end 
